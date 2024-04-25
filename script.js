ab26715be19cdee399230ab24b0dc0b093cfbe4c
Pooja Shree D R <poojashreeraguram@gmail.com>
2024-04-25 20:45:09 +0530

    First commit

--- script.js
+++ script.js
@@ -2,8 +2,17 @@ const express=require('express');
 const dotenv=require('dotenv').config();
 const mongoose=require('mongoose');
 const app=express();
+const path=require('path');
 const mongoURI=process.env.MONGO_URI;
+app.use(express.json());
+app.use(express.static(path.join(__dirname,'public')))
 mongoose.connect(mongoURI)
 .then(()=>console.log('Connected to MongoDB'))
 .catch(err=>console.error('Error connecting to MongoDB:',err));
+const userSchema=new mongoose.Schema({
+    name: String,
+    email: String,
+    password:String
+});
+const User =mongoose.model('User',userSchema);
 app.listen(process.env.PORT,()=>console.log("Connected to server"));
